883ee3f96edc70f1957c29be71f6cb22b9091e9d
Ramil Burgart <ramilburgart7006@gmail.com>
2024-09-29 18:22:08 +0300

    homework four

---
+++ src/main/java/org/homeworkfour/Main.java
@@ -0,0 +1,10 @@
+package org.homeworkfour;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String stringLetter = "poiuytrewqlkjhgfdsamnbvcxz";
+        SortedLetter sortedLetter = new SortedLetter();
+        sortedLetter.splittingString(stringLetter);
+    }
+}
